94e69c58f058638b25d167628770c6571db761a0
Tonmoy Roy <rxdevs100@gmail.com>
2022-10-10 21:31:43 +0600

    drum kit functionality added & audio played

--- Day1_JavaScript_Drum_Kit/js/main.js
+++ Day1_JavaScript_Drum_Kit/js/main.js
@@ -1,1 +1,21 @@
 console.log('Drum Kit');
+
+
+// keydown event
+window.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+
+    // select audio
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // select key
+    const key = document.querySelector(`div[data-key="${e.keyCode}"`);
+
+    // add 'playing' classg
+    key.classList.add('playing');
+
+    // play audio
+    audio.play();
+    
+});
+
+
